283d8f7f7f12078829ab74594a7cdedc270eab23
Rac <rac75116@gmail.com>
2025-04-24 23:36:21 +0900

    ヘッダを更新

--- src/app/(header)/header.tsx
+++ src/app/(header)/header.tsx
@@ -1,14 +1,42 @@
-import { Hamburger } from "./hamburger";
-import NoSsr from "./no-ssr";
+"use client";
+import { DotLottie, DotLottieReact } from "@lottiefiles/dotlottie-react";
+import Link from "next/link";
+import { useState } from "react";
 import Theme from "./theme.svg";
 
 export function Header() {
+    const [open, setOpen] = useState(false);
+    const [hamburger, setHamburger] = useState<DotLottie | null>(null);
     return (
-        <header className={`flex h-16 items-center bg-white`}>
-            <Theme className={`mr-auto h-1/2 pl-5`} />
-            <NoSsr>
-                <Hamburger />
-            </NoSsr>
+        <header className="flex h-20 items-center bg-white">
+            <Theme className="mr-auto h-35/134 pl-7" />
+            <Link href="/2025" className="mr-5 ml-5">
+                Top
+            </Link>
+            <Link href="/2025/theme-logo" className="mr-5 ml-5">
+                Theme & Logo
+            </Link>
+            <Link href="/2025/news" className="mr-5 ml-5">
+                News
+            </Link>
+            <Link href="/2025/access" className="mr-5 ml-5">
+                Access
+            </Link>
+            <button
+                className="mr-7 aspect-1/1 h-88/134 cursor-pointer"
+                onClick={() => {
+                    hamburger?.setMode(open ? "reverse" : "forward");
+                    hamburger?.play();
+                    setOpen(!open);
+                }}
+            >
+                <div className="aspect-2/1">
+                    <DotLottieReact
+                        src="/2025/animations/hamburger.lottie"
+                        dotLottieRefCallback={(ref) => setHamburger(ref)}
+                    />
+                </div>
+            </button>
         </header>
     );
 }
