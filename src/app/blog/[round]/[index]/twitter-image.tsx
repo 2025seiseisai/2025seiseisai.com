d64f4745d7b50f1f29c883938122f88810364034
Rac <rac75116@gmail.com>
2025-05-02 00:05:27 +0900

    パフォーマンス改善

--- src/app/blog/[round]/[index]/twitter-image.tsx
+++ src/app/blog/[round]/[index]/twitter-image.tsx
@@ -1,5 +1,9 @@
+import { enumetateParams } from "@/blogs/blog-impl";
 import OpenGraphImage, { alt as og_alt, contentType as og_ct, size as og_size } from "./opengraph-image";
 export const alt = og_alt;
 export const size = og_size;
 export const contentType = og_ct;
 export default OpenGraphImage;
+export function generateStaticParams() {
+    return enumetateParams();
+}
