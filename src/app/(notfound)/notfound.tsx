af81faf363ba0539264606d6256205304a879f0e
okonomiyaki-chaos <unagidango.dism@gmail.com>
2025-05-02 08:27:27 +0900

    404 作業開始

--- src/app/(notfound)/notfound.tsx
+++ src/app/(notfound)/notfound.tsx
@@ -3,8 +3,12 @@ import styles from "./page.module.scss";
 export function NotFound() {
     return (
         <>
-            {/* ここに書く */}
-            <p className={styles.example}>404 Not Found.</p>
+            <p className={styles.title}>
+                <span className={styles.red}>4</span>04
+            </p>
+            <p className={styles.subtitle}>
+                <span className={styles.red}>P</span>age Not Found
+            </p>
         </>
     );
 }
