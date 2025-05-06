489a81bfc06fa0266a481a25a699b5fe82404b08
Rac <rac75116@gmail.com>
2025-05-06 12:18:48 +0900

    SCSSとか修正

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     sassOptions: {
-        additionalData: `@use "@/impl/_global.scss" as *;`,
+        additionalData: `@use "@/impl/_global" as *;`,
     },
     turbopack: {
         rules: {
