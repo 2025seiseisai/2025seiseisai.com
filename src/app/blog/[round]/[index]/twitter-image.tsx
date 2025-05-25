c3bdbc376fcfc9eb083fee9a96bbbd83cd1cb4ca
Rac <rac75116@gmail.com>
2025-05-25 11:35:09 +0900

    不必要なフォルダを削除

--- src/app/blog/[round]/[index]/twitter-image.tsx
+++
@@ -1,9 +0,0 @@
-import { enumetateParams } from "@/impl/blog";
-import OpenGraphImage, { alt as og_alt, contentType as og_ct, size as og_size } from "./opengraph-image";
-export const alt = og_alt;
-export const size = og_size;
-export const contentType = og_ct;
-export default OpenGraphImage;
-export function generateStaticParams() {
-    return enumetateParams();
-}
