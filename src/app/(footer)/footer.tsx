f8ea443783c824ad11e7b3c5c0aaed2392f591e2
Rac <rac75116@gmail.com>
2025-05-26 17:23:53 +0900

    誤字修正

--- src/app/(footer)/footer.tsx
+++ src/app/(footer)/footer.tsx
@@ -76,7 +76,7 @@ export function Footer() {
                     <DisabledFooterLink>Blog</DisabledFooterLink>
                     <DisabledFooterLink>Special</DisabledFooterLink>
                     <DisabledFooterLink>Downloads</DisabledFooterLink>
-                    <DisabledFooterLink>Gellery</DisabledFooterLink>
+                    <DisabledFooterLink>Gallery</DisabledFooterLink>
                 </span>
                 <span className="flex flex-row not-md:mt-[16px] not-md:mr-[17px] not-md:mb-[16px] not-md:w-full not-md:justify-end not-md:gap-[5px] md:mt-auto md:mr-[calc(7900svw/1440)] md:mb-[30px] md:ml-auto md:flex-col md:gap-[8px] lg:flex-row">
                     <Link
