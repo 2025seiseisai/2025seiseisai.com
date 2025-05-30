c2e205c385a5d93302c3ba26e61cc968a391a58d
Rac <rac75116@gmail.com>
2025-05-31 00:02:52 +0900

    blog一覧修正

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,5 @@
 import { getAllBlogs } from "@/impl/blog";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./page.module.scss";
+import BlogList from "./list";
 
 export const metadata = {
     title: "Blog | 第61回菁々祭「分秒」 - 東大寺学園文化祭2025",
@@ -10,25 +8,19 @@ export const metadata = {
 export default async function Page() {
     return (
         <>
-            {/* ここに書く */}
-            <p className={styles.example}>ブログ一覧</p>
-            {/* 実装の一例 */}
-            {/* getAllBlogs()でブログの情報を全て取得できる */}
-            {getAllBlogs().map((blog) => {
-                // blog.roundで第何回の菁々祭のブログか判別できる
-                return (
-                    <div key={`${blog.round}/${blog.index}`}>
-                        {/* サムネイルはgetThumnailをImageのsrcに渡すことで表示できる */}
-                        <Image src={blog.thumbnail} alt="thumbnail" />
-                        <h1>{blog.title}</h1>
-                        <p>{blog.date}</p>
-                        <p>{blog.author}</p>
-                        <p>{blog.topic}</p>
-                        {/* リンクはLinkを使う */}
-                        <Link href={`/2025/blog/${blog.round}/${blog.index}`}>Read more</Link>
-                    </div>
-                );
-            })}
+            <BlogList
+                blogs={getAllBlogs().map(({ round, index, title, date, author, topic, thumbnail }) => {
+                    return {
+                        round,
+                        index,
+                        title,
+                        date,
+                        author,
+                        topic,
+                        thumbnail,
+                    };
+                })}
+            />
         </>
     );
 }
