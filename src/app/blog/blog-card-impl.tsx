6e9d276256c1c9daffa9c6f6068c5931d2870d8d
Rac <rac75116@gmail.com>
2025-06-15 22:28:20 +0900

    調整

--- src/app/blog/blog-card-impl.tsx
+++ src/app/blog/blog-card-impl.tsx
@@ -4,12 +4,22 @@ import spinnerStyles from "./spinner.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function BlogCardImpl({ round, index, blog }: { round: string; index: string; blog?: BlogMetadata }) {
+export default function BlogCardImpl({
+    round,
+    index,
+    blog,
+    width = "w-[min(350px,calc(100svw-40px))]",
+}: {
+    round: string;
+    index: string;
+    blog?: BlogMetadata;
+    width: string;
+}) {
     return (
         <Link
             href={`/2025/blog/${round}/${index}`}
             prefetch={false}
-            className="@container relative flex h-85 w-87.5 max-w-[calc(100svw_-_40px_-_10dvw)] flex-initial flex-row flex-wrap justify-center overflow-hidden rounded-[0.625rem] border-2 border-pri-red bg-[#f7f7f7] transition duration-500 ease-in-out hover:scale-102 hover:opacity-90"
+            className={`${width} @container relative flex h-[350px] flex-initial flex-col items-center justify-between overflow-hidden rounded-[0.625rem] border-2 border-pri-red bg-[#f7f7f7] transition duration-500 ease-in-out hover:scale-102 hover:opacity-90`}
         >
             {!blog && (
                 <div className="absolute flex h-full w-full items-center justify-center">
@@ -21,25 +31,25 @@ export default function BlogCardImpl({ round, index, blog }: { round: string; in
                     <Image
                         src={blog.thumbnail}
                         alt="thumbnail"
-                        className="aspect-16/9 h-[150px] w-full object-cover @[18.75rem]:h-auto"
+                        className="h-[160px] w-full object-cover @[18.75rem]:h-[195px]"
                     />
-                    {round !== "61" && blog && (
-                        <div className="absolute top-1 right-1 flex aspect-square size-max items-center justify-center rounded-full border-1 border-solid border-pri-red bg-[#ffffff]/80 p-1 text-base text-pri-red">
-                            過去
+                    <section className="flex h-[186px] w-5/6 flex-col items-center justify-center gap-[3px] @[18.75rem]:h-full">
+                        {round !== "61" && blog && (
+                            <div className="absolute top-[8px] right-[8px] flex aspect-square size-max items-center justify-center rounded-full border-1 border-solid border-pri-red bg-[#ffffff]/80 p-1 text-base text-pri-red">
+                                過去
+                            </div>
+                        )}
+                        <div className="flex w-full flex-col items-start justify-between @[18.75rem]:flex-row @[18.75rem]:items-center">
+                            <p className="w-max text-[16px] text-pri-red">{blog.date}</p>
+                            <p className="max-w-[100%] truncate rounded-[5px] bg-pri-red px-[6px] text-[14px]/normal text-[#ffffff] @[18.75rem]:max-w-[62%]">
+                                ＃{blog.topic}
+                            </p>
                         </div>
-                    )}
-                    <div className="mt-2.5 mr-[8.33333%] ml-[8.33333%] flex w-5/6 flex-col items-start justify-between @[18.75rem]:mt-3.75 @[18.75rem]:flex-row @[18.75rem]:items-center">
-                        <p className="h-6 w-[100px] text-base text-pri-red">{blog.date}</p>
-                        <p className="max-w-[100%] truncate rounded-[0.3125rem] bg-pri-red px-1.5 text-sm/normal text-[#ffffff] @[18.75rem]:max-w-[calc(100%-100px)]">
-                            ＃{blog.topic}
+                        <p className="line-clamp-3 h-[81px] w-full text-[18px]/normal text-pri-black @[18.75rem]:line-clamp-2 @[18.75rem]:h-[66px] @[18.75rem]:text-[22px]/normal">
+                            {blog.title}
                         </p>
-                    </div>
-                    <p className="line-clamp-3 h-22.5 w-5/6 text-left text-xl/normal text-pri-black @[18.75rem]:line-clamp-2 @[18.75rem]:h-15">
-                        {blog.title}
-                    </p>
-                    <p className="mr-[8.33333%] mb-3.75 ml-auto w-max max-w-2/3 truncate text-[0.9375rem]/normal text-pri-black">
-                        {blog.author}
-                    </p>
+                        <p className="w-full truncate text-end text-[15px]/normal text-pri-black">{blog.author}</p>
+                    </section>
                 </>
             )}
         </Link>
