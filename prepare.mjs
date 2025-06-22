7061c96b09954b3a788ff6110625f29ccdafae91
Rac <rac75116@gmail.com>
2025-06-22 09:14:44 +0900

    自動でvscode拡張をインストール

--- prepare.mjs
+++ prepare.mjs
@@ -1,5 +1,25 @@
 import childProcess from "child_process";
 import fs from "fs";
+import extensions from "./.vscode/extensions.json" with { type: "json" };
+childProcess.exec("code --list-extensions", (error, stdout) => {
+    if (error) {
+        console.error("Error listing extensions:", error);
+        return;
+    }
+    const installedExtensions = stdout.split("\n").filter((ext) => ext.trim() !== "");
+    for (const extension of extensions.recommendations) {
+        if (installedExtensions.includes(extension)) {
+            continue;
+        }
+        childProcess.exec(`code --install-extension ${extension}`, (error) => {
+            if (error) {
+                console.error(`Error installing extension ${extension}:`, error);
+            } else {
+                console.log(`Extension ${extension} installed successfully.`);
+            }
+        });
+    }
+});
 if (process.env.AUTH_SECRET === undefined) {
     childProcess.exec("npx auth secret --raw", (error, stdout) => {
         if (error) {
