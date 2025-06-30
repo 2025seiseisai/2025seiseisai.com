03d254a9b7fb8ee4ced0da8c91fd7208b110a9e3
Rac <rac75116@gmail.com>
2025-06-30 16:11:25 +0900

    修正

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { getAllBlogs } from "@/impl/blog";
 import NewsManager from "@/impl/news";
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const defaultPages = [
         {
