d581f86937da1b9467826b6531c14e07ef2c15c1
Rac <rac75116@gmail.com>
2025-07-15 17:46:11 +0900

    いろいろ説明を追加 & 修正

--- src/app/special/page.tsx
+++ src/app/special/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.scss";
 
 export const metadata = {
@@ -9,6 +11,38 @@ export default function Page() {
         <>
             {/* ここに書く */}
             <p className={styles.example}>スペシャル</p>
+            {/* Special用画像の張り方 */}
+            <Image
+                src="/2025/special/wallpaper/1.png" // 1.png~5.pngまであります
+                alt="壁紙"
+                width={180}
+                height={360}
+            />
+            <Image
+                src="/2025/special/icon/2.png" // 1.png~4.pngまであります
+                alt="アイコン"
+                width={150}
+                height={150}
+            />
+            <Image
+                src="/2025/special/header/3.png" // 1.png~4.pngまであります
+                alt="ヘッダー"
+                width={495}
+                height={165}
+            />
+            {/* ダウンロードボタンの作り方 */}
+            {/* ボタンにしたい要素をLinkで囲う */}
+            <Link href="/special/wallpaper/4.png" download>
+                <p>壁紙4をダウンロード</p>
+            </Link>
+            {/* 等間隔に並べたい場合は以下のようにするといい感じになります */}
+            <div className={styles.flexContainer}>
+                <p>あ</p>
+                <p>い</p>
+                <p>う</p>
+                <p>え</p>
+                <p>お</p>
+            </div>
         </>
     );
 }
