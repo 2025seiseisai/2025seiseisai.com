35c972528b640caca0caa33581758c6b6240d64b
mofmof2974 <leonmail2974@gmail.com>
2025-08-06 02:17:58 +0900

    変更

--- src/app/events/event-data.ts
+++ src/app/events/event-data.ts
@@ -12,31 +12,11 @@ export type EventData = {
         end: `${HourType}:${MinuteType}`;
     }[];
 };
-export const locations = [
-    "中庭",
-    "体育館",
-    "転心殿前",
-    "圓融館",
-    "小講堂",
-    "視聴覚室",
-    "4A",
-    "音楽室",
-    "2B",
-    "3A",
-    "3B",
-    "4E",
-    "5E",
-    "情報教室",
-    "物理室",
-    "地学室",
-    "上グラウンド",
-    "お化け屋敷",
-    "pending",
-] as const;
+export const locations = ["中庭","体育館","転心殿前","圓融館","小講堂","視聴覚室","4A","音楽室","2B","3A","3B","4E","5E","情報教室","物理室","地学室","上グラウンド","お化け屋敷","pending"] as const;
 export const eventData: EventData[] = [
     {
         name: "かわいいだけじゃだめですか？",
-        description: "CUTIE STREETの、かわいいだけじゃだめですか？を踊ります",
+        description:"CUTIE STREETの、かわいいだけじゃだめですか？を踊ります",
         ticket: "pending",
         events: [
             {
@@ -50,8 +30,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "レゴプログラミング講習会",
-        description:
-            "レゴで作られたロボットを動かすプログラミングを体験してもらいます。地学室を使って2日とも1時間の講習を2回ずつ行います。一回の講習で5人までの参加が可能です。",
+        description:"レゴで作られたロボットを動かすプログラミングを体験してもらいます。地学室を使って2日とも1時間の講習を2回ずつ行います。一回の講習で5人までの参加が可能です。",
         ticket: true,
         events: [
             {
@@ -82,7 +61,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "無差別級ビブリオバトル",
-        description: "紹介するものが広義の本なビブリオバトル",
+        description:"紹介するものが広義の本なビブリオバトル",
         ticket: "pending",
         events: [
             {
@@ -101,7 +80,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "釈迦釈迦BOYS",
-        description: "バンドをします。",
+        description:"バンドをします。",
         ticket: "pending",
         events: [
             {
@@ -109,12 +88,12 @@ export const eventData: EventData[] = [
                 location: "pending",
                 start: "?:?",
                 end: "?:?",
-            },
+            }
         ],
     },
     {
         name: "音楽部ライブ",
-        description: "音楽部によるバンド演奏",
+        description:"音楽部によるバンド演奏",
         ticket: "pending",
         events: [
             {
@@ -142,31 +121,31 @@ export const eventData: EventData[] = [
                 start: "10:50",
                 end: "11:30",
             },
-            {
+                        {
                 day: 1,
                 location: "小講堂",
                 start: "12:40",
                 end: "13:20",
             },
-            {
+                                    {
                 day: 1,
                 location: "小講堂",
                 start: "14:10",
                 end: "14:50",
             },
-            {
+                                    {
                 day: 2,
                 location: "小講堂",
                 start: "10:50",
                 end: "11:30",
             },
-            {
+                        {
                 day: 2,
                 location: "小講堂",
                 start: "12:40",
                 end: "13:20",
             },
-            {
+                                    {
                 day: 2,
                 location: "小講堂",
                 start: "14:10",
@@ -176,12 +155,12 @@ export const eventData: EventData[] = [
     },
     {
         name: "和太鼓演奏",
-        description: "和太鼓を演奏します",
+        description:"和太鼓を演奏します",
         ticket: false,
         events: [
             {
                 day: 1,
-                location: "転心殿前",
+                location:"転心殿前",
                 start: "09:00",
                 end: "09:50",
             },
@@ -193,7 +172,7 @@ export const eventData: EventData[] = [
             },
             {
                 day: 2,
-                location: "転心殿前",
+                location:"転心殿前",
                 start: "09:00",
                 end: "09:50",
             },
@@ -207,7 +186,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "スリーレッドメン",
-        description: "有志バンド",
+        description:"有志バンド",
         ticket: "pending",
         events: [
             {
@@ -220,7 +199,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "江南無スタイル",
-        description: "カンナムスタイルを踊る",
+        description:"カンナムスタイルを踊る",
         ticket: "pending",
         events: [
             {
@@ -233,7 +212,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "室内楽部定期演奏会",
-        description: "室内楽部による圓融館での演奏会",
+        description:"室内楽部による圓融館での演奏会",
         ticket: "pending",
         events: [
             {
@@ -252,7 +231,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "菁々コンサート",
-        description: "有志の生徒が、圓融館で、曲を演奏する。",
+        description:"有志の生徒が、圓融館で、曲を演奏する。",
         ticket: "pending",
         events: [
             {
@@ -271,7 +250,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "やさしい落語",
-        description: "現代口語で落語をする",
+        description:"現代口語で落語をする",
         ticket: "pending",
         events: [
             {
@@ -284,7 +263,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "TDJ48",
-        description: "坂道系を中心とした曲を女装して踊ります。",
+        description:"坂道系を中心とした曲を女装して踊ります。",
         ticket: false,
         events: [
             {
@@ -315,7 +294,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "ロケット発射実験",
-        description: "上グラウンドでロケットを打ち上げる",
+        description:"上グラウンドでロケットを打ち上げる",
         ticket: false,
         events: [
             {
@@ -346,7 +325,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "ラブライブ！サンスクリット‼︎",
-        description: "ラブライブシリーズのコピーダンスユニットです。",
+        description:"ラブライブシリーズのコピーダンスユニットです。",
         ticket: false,
         events: [
             {
@@ -377,8 +356,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "T(DJ)²",
-        description:
-            "東大寺生によるDJイベントです。ジャンル問わず様々な曲を流します。1日数名程度、持ち時間は30〜50分を想定しています。",
+        description:"東大寺生によるDJイベントです。ジャンル問わず様々な曲を流します。1日数名程度、持ち時間は30〜50分を想定しています。",
         ticket: "pending",
         events: [
             {
@@ -403,7 +381,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "マッスルコンテスト",
-        description: "色々な種目により、東大寺学園の筋肉王を決める。1対1で勝ち上がりで行う。",
+        description:"色々な種目により、東大寺学園の筋肉王を決める。1対1で勝ち上がりで行う。",
         ticket: false,
         events: [
             {
@@ -416,8 +394,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "E卍ILE",
-        description:
-            "BTSをはじめとしたK-POPの楽曲で、キレのあるパフォーマンスをお届けします! ダンス、表情、フォーメーション——細部までこだわって仕上げたステージは必見。憧れのアーティストに少しでも近づけるよう、全力で練習してきました。 一緒に盛り上がってください！応援よろしくお願いします！",
+        description:"BTSをはじめとしたK-POPの楽曲で、キレのあるパフォーマンスをお届けします! ダンス、表情、フォーメーション——細部までこだわって仕上げたステージは必見。憧れのアーティストに少しでも近づけるよう、全力で練習してきました。 一緒に盛り上がってください！応援よろしくお願いします！",
         ticket: false,
         events: [
             {
@@ -448,7 +425,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "異次元フェス",
-        description: "マスとラ！の合同イベント",
+        description:"マスとラ！の合同イベント",
         ticket: "pending",
         events: [
             {
@@ -461,7 +438,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "クイズ研究部",
-        description: "クイズ大会他校のクイズ研究部を招待し、早押しクイズなどを行って優勝チームを決める。",
+        description:"クイズ大会他校のクイズ研究部を招待し、早押しクイズなどを行って優勝チームを決める。",
         ticket: "pending",
         events: [
             {
@@ -474,7 +451,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "プレゼン、ゲーム大会の二種類",
-        description: "プログラミングに関するプレゼンと、部員の自作ゲームのゲーム大会",
+        description:"プログラミングに関するプレゼンと、部員の自作ゲームのゲーム大会",
         ticket: "pending",
         events: [
             {
@@ -487,7 +464,7 @@ export const eventData: EventData[] = [
     },
     {
         name: "寺マス",
-        description: "アイドルマスターズの曲を女装して踊ります",
+        description:"アイドルマスターズの曲を女装して踊ります",
         ticket: false,
         events: [
             {
