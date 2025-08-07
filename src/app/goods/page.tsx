c23c1bc3b032fdb0ffd0a28eff69d2b3db46fd7b
Naf-Kasa <kasasagi3d@gmail.com>
2025-08-07 18:27:52 +0900

    01

--- src/app/goods/page.tsx
+++ src/app/goods/page.tsx
@@ -9,6 +9,17 @@ export default function Page() {
         <>
             {/* ここに書く */}
             <p className={styles.example}>グッズ</p>
+            <h1
+                className="mt-[30px] mb-[25px] ml-[20px] text-[28px] font-bold md:mb-[30px] md:ml-[10svw]
+                    md:text-[40px]"
+            >
+                <span className="text-[#de0d22]">G</span>oods
+            </h1>
+            <div className={styles.goods}>
+                <div className={styles.f523}></div>
+                <div className={styles.f511}></div>
+                <div className={styles.f529}></div>
+            </div>
         </>
     );
 }
