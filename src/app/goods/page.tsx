67e9d9b09fe8c5e1814cf3a08d0fbca34c33f641
Naf-Kasa <kasasagi3d@gmail.com>
2025-08-09 11:11:43 +0900

    完成(グッズの個別設定はまだ)

--- src/app/goods/page.tsx
+++ src/app/goods/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import tshirt from "./img/t.png";
 import styles from "./page.module.scss";
 
-import tshirt from "./img/t.png";
-
-const back = "/img/back.png";
+import back from "./img/back.png";
 
 export const metadata = {
     title: "Goods | 第61回菁々祭「分秒」 - 東大寺学園文化祭2025",
@@ -12,7 +11,6 @@ export const metadata = {
 export default function Page() {
     return (
         <>
-            {/* ここに書く */}
             <h1
                 className="mt-[30px] mb-[25px] ml-[20px] text-[28px] font-bold md:mb-[30px] md:ml-[10svw]
                     md:text-[40px]"
@@ -21,98 +19,120 @@ export default function Page() {
             </h1>
             <div className="md:mb-[50px]">
                 <div className={styles.container}>
+                    {/* 商品 */}
                     <div className={styles.goods}>
                         <div className={styles.f523}>
-                            <h2 className={styles.name}>Tシャツ</h2>
+                            <h2 className={styles.name}>Tシャツ1</h2>
                             <h2 className={styles.price}>900円</h2>
                         </div>
                         <div className={styles.f511}>在庫あり</div>
-
                         <div
                             className={styles.wrapper}
-                            style={{
-                                background: `url(${back}) no-repeat center/cover`,
-                            }}
+                            style={
+                                {
+                                    "--bg-img": `url(${back.src})`,
+                                } as React.CSSProperties
+                            }
                         >
-                            <div className={styles.squareImage} style={{ position: "relative" }}>
-                                <Image src={tshirt} alt="Tシャツ" fill style={{ objectFit: "cover" }} />
+                            <div className={styles.squareImage}>
+                                <Image src={tshirt} alt="Tシャツ1" fill style={{ objectFit: "cover" }} />
                             </div>
                         </div>
+                        <p className={styles.desc}>
+                            第61回菁々祭のオリジナルTシャツです。S,M,L,XLのサイズをご用意しております。
+                        </p>
                     </div>
                     <div className={styles.goods}>
                         <div className={styles.f523}>
-                            <h2 className={styles.name}>Tシャツ</h2>
+                            <h2 className={styles.name}>Tシャツ2</h2>
                             <h2 className={styles.price}>900円</h2>
                         </div>
                         <div className={styles.f511}>在庫あり</div>
-
                         <div
                             className={styles.wrapper}
-                            style={{
-                                background: `url(${back}) no-repeat center/cover`,
-                            }}
+                            style={
+                                {
+                                    "--bg-img": `url(${back.src})`,
+                                } as React.CSSProperties
+                            }
                         >
-                            <div className={styles.squareImage} style={{ position: "relative" }}>
-                                <Image src={tshirt} alt="Tシャツ" fill style={{ objectFit: "cover" }} />
+                            <div className={styles.squareImage}>
+                                <Image src={tshirt} alt="Tシャツ1" fill style={{ objectFit: "cover" }} />
                             </div>
                         </div>
+                        <p className={styles.desc}>
+                            第61回菁々祭のオリジナルTシャツです。S,M,L,XLのサイズをご用意しております。
+                        </p>
                     </div>
                     <div className={styles.goods}>
                         <div className={styles.f523}>
-                            <h2 className={styles.name}>Tシャツ</h2>
+                            <h2 className={styles.name}>Tシャツ3</h2>
                             <h2 className={styles.price}>900円</h2>
                         </div>
                         <div className={styles.f511}>在庫あり</div>
-
                         <div
                             className={styles.wrapper}
-                            style={{
-                                background: `url(${back}) no-repeat center/cover`,
-                            }}
+                            style={
+                                {
+                                    "--bg-img": `url(${back.src})`,
+                                } as React.CSSProperties
+                            }
                         >
-                            <div className={styles.squareImage} style={{ position: "relative" }}>
-                                <Image src={tshirt} alt="Tシャツ" fill style={{ objectFit: "cover" }} />
+                            <div className={styles.squareImage}>
+                                <Image src={tshirt} alt="Tシャツ1" fill style={{ objectFit: "cover" }} />
                             </div>
                         </div>
+                        <p className={styles.desc}>
+                            第61回菁々祭のオリジナルTシャツです。S,M,L,XLのサイズをご用意しております。
+                        </p>
                     </div>
                     <div className={styles.goods}>
                         <div className={styles.f523}>
-                            <h2 className={styles.name}>Tシャツ</h2>
+                            <h2 className={styles.name}>Tシャツ4</h2>
                             <h2 className={styles.price}>900円</h2>
                         </div>
                         <div className={styles.f511}>在庫あり</div>
-
                         <div
                             className={styles.wrapper}
-                            style={{
-                                background: `url(${back}) no-repeat center/cover`,
-                            }}
+                            style={
+                                {
+                                    "--bg-img": `url(${back.src})`,
+                                } as React.CSSProperties
+                            }
                         >
-                            <div className={styles.squareImage} style={{ position: "relative" }}>
-                                <Image src={tshirt} alt="Tシャツ" fill style={{ objectFit: "cover" }} />
+                            <div className={styles.squareImage}>
+                                <Image src={tshirt} alt="Tシャツ1" fill style={{ objectFit: "cover" }} />
                             </div>
                         </div>
+                        <p className={styles.desc}>
+                            第61回菁々祭のオリジナルTシャツです。S,M,L,XLのサイズをご用意しております。
+                        </p>
                     </div>
                     <div className={styles.goods}>
                         <div className={styles.f523}>
-                            <h2 className={styles.name}>Tシャツ</h2>
+                            <h2 className={styles.name}>Tシャツ5</h2>
                             <h2 className={styles.price}>900円</h2>
                         </div>
                         <div className={styles.f511}>在庫あり</div>
-
                         <div
                             className={styles.wrapper}
-                            style={{
-                                background: `url(${back}) no-repeat center/cover`,
-                            }}
+                            style={
+                                {
+                                    "--bg-img": `url(${back.src})`,
+                                } as React.CSSProperties
+                            }
                         >
-                            <div className={styles.squareImage} style={{ position: "relative" }}>
-                                <Image src={tshirt} alt="Tシャツ" fill style={{ objectFit: "cover" }} />
+                            <div className={styles.squareImage}>
+                                <Image src={tshirt} alt="Tシャツ1" fill style={{ objectFit: "cover" }} />
                             </div>
                         </div>
+                        <p className={styles.desc}>
+                            第61回菁々祭のオリジナルTシャツです。S,M,L,XLのサイズをご用意しております。
+                        </p>
                     </div>
                 </div>
             </div>
+
             <div className={styles.f504}>
                 <div className={styles.m}>
                     <h2 className={styles.zaiko}>在庫について</h2>
