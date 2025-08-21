f736bb23b89f92e35a9582688276c751b818dea9
Rac <rac75116@gmail.com>
2025-08-22 01:57:49 +0900

    修正

--- src/app/(header)/hamburger.tsx
+++ src/app/(header)/hamburger.tsx
@@ -13,7 +13,8 @@ export default function Hamburger({ open, setOpen }: { open: boolean; setOpen: (
     return (
         <button
             className={`z-100000002 flex aspect-1/1 h-[30px] w-auto cursor-pointer items-center justify-center
-                transition-all duration-500 outline-none md:h-[40px] ${open ? styles.opening : "mr-[min(35px,7svw)]"}`}
+                transition-all duration-500 outline-none md:h-[40px]
+                ${open ? styles.opening : "mr-[min(35px,7svw)] md:mr-[min(40px,8svw)]"}`}
             onClick={() => {
                 setTimeout(() => {
                     setOpen(!open);
