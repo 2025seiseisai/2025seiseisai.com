599444fe8c1b187e77172d300f716f4fcd1b31c6
ebaaoi <eba-aoi@hotmail.com>
2025-08-23 21:32:36 +0900

    追加

--- src/app/map/(exhibition)/exhibition.tsx
+++ src/app/map/(exhibition)/exhibition.tsx
@@ -10,19 +10,44 @@ function Bazaar() {
 
 export function Exhibition1({ floor }: { floor: ExhibitionFloor }) {
     return (
-        <>
-            {/* ここに書く */}
+        <div className={styles.exhibitionContainer}>
+            {/* ここに書く */};
             <p className={styles.example}>エキシビション1</p>
             <p>現在のフロア: {floor}</p>
-        </>
+        </div>
     );
 }
 
 export function Exhibition2() {
     return (
-        <>
-            {/* ここに書く */}
-            <p className={styles.example}>エキシビション2</p>
-        </>
+        <div className={styles.tagContainer}>
+            <div>
+                <p>各種タグについて</p>
+            </div>
+            <div className={styles.tagstamp}>
+                <div className={styles.tagiconstamp}>
+                    <p>スタンプラリー</p>
+                </div>
+                <div className={styles.tagtextstamp}>
+                    <p>スタンプラリーを行っている団体です。</p>
+                </div>
+            </div>
+            <div className={styles.tagevents}>
+                <div className={styles.tagiconevents}>
+                    <p>イベント</p>
+                </div>
+                <div className={styles.tagtextevents}>
+                    <p>イベントを行っている団体です。詳しくは<span style={{ textDecoration: 'underline' }}>Evens</span>をチェック！</p>
+                </div>
+            </div>
+            <div className={styles.tagbazaar}>
+                <div className={styles.tagiconbazaar}>
+                    <p>バザー</p>
+                </div>
+                <div className={styles.tagtextbazaar}>
+                    <p>バザーでの販売を行っている団体です。</p>
+                </div>
+            </div>
+        </div>
     );
 }
