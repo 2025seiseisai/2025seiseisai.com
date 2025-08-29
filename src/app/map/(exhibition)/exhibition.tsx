3f400d6d30e73f75318772f431bd644484aa5312
ebaaoi <eba-aoi@hotmail.com>
2025-08-29 21:49:55 +0900

    tuika

--- src/app/map/(exhibition)/exhibition.tsx
+++ src/app/map/(exhibition)/exhibition.tsx
@@ -1,23 +1,23 @@
-import { ExhibitionFloor, locations, exhibitionData } from "./exhibition-data";
-import styles from "./page.module.scss";
 import { exhibitionIcons } from "../(exhibition)/exhibition-icons"; // ファイルの先頭でimport
-import Link from "next/link";
-function exhibition() {
+
+function Bazaar() {
     // SVGがdivで囲まれて埋め込まれる
     // divの大きさを変えることでSVGの大きさも変わる
-    return <div dangerouslySetInnerHTML={{ __html: exhibitionIcons["PRパート"] }} />
+    return <div dangerouslySetInnerHTML={{ __html: exhibitionIcons["PRパート"] }} />;
 }
+import { ExhibitionFloor, locations, exhibitionData } from "./exhibition-data";
+import styles from "./page.module.scss";
+import Link from "next/link";
 
 export function Exhibition1({ floor }: { floor: ExhibitionFloor }) {
-    const filteredExs =
-    Object.entries(exhibitionData).filter(
-        ([, data]) => locations[data.location] === floor
-        );
+    const filteredExs = Object.entries(exhibitionData).filter(([, data]) => locations[data.location] === floor);
     return (
         <div>
             {filteredExs.map(([name, data]) => (
                 <div key={name} className={styles.card}>
-                    <div className={styles.icon} />
+                    <div className={styles.icon}>
+                        <img src={data.icon} alt={`${name} icon`} className={styles.iconImage} />
+                    </div>
                     <div className={styles.content}>
                         <div className={styles.location}>{data.location}</div>
                         <div className={styles.title}>{name}</div>
@@ -27,8 +27,8 @@ export function Exhibition1({ floor }: { floor: ExhibitionFloor }) {
                             <button className={styles.bazaarbutton}>バザー</button>
                         </div>
                     </div>
-                    <Link href="https://seiseisai.com/"  target="_blank" rel="noopener noreferrer">
-                        <div className={styles.arrow}>➡️</div>
+                    <Link href="https://seiseisai.com/" target="_blank" rel="noopener noreferrer">
+                        <div className={styles.arrow}>→</div>
                     </Link>
                 </div>
             ))}
@@ -37,36 +37,35 @@ export function Exhibition1({ floor }: { floor: ExhibitionFloor }) {
 }
 
 const tags = [
-  {
-    label: 'スタンプラリー',
-    color: 'red',
-    description: 'スタンプラリーを行っている団体です。',
-  },
-  {
-    label: 'イベント',
-    color: 'orange',
-    description: (
-    <>
-      イベントを行っている団体です。詳しくは{' '}
-      <a
-        href="http://localhost:3000/2025"
-        className={styles.highlight}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Events
-      </a>{' '}
-      をチェック！
-    </>
-  ),
-  },
-  {
-    label: 'バザー',
-    color: 'yellow',
-    description: 'バザーでの販売を行っている団体です。',
-  },
+    {
+        label: "スタンプラリー",
+        color: "red",
+        description: "スタンプラリーを行っている団体です。",
+    },
+    {
+        label: "イベント",
+        color: "orange",
+        description: (
+            <>
+                イベントを行っている団体です。詳しくは{" "}
+                <a
+                    href="http://localhost:3000/2025"
+                    className={styles.highlight}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Events
+                </a>{" "}
+                をチェック！
+            </>
+        ),
+    },
+    {
+        label: "バザー",
+        color: "yellow",
+        description: "バザーでの販売を行っている団体です。",
+    },
 ];
-
 
 export function Exhibition2() {
     return (
