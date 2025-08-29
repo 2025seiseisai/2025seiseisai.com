2714089d5cecf7d3194daf7700b55936efd8e523
ebaaoi <eba-aoi@hotmail.com>
2025-08-29 19:53:49 +0900

    tuikia

--- src/app/map/(exhibition)/exhibition.tsx
+++ src/app/map/(exhibition)/exhibition.tsx
@@ -2,7 +2,7 @@ import { ExhibitionFloor, locations, exhibitionData } from "./exhibition-data";
 import styles from "./page.module.scss";
 import { exhibitionIcons } from "../(exhibition)/exhibition-icons"; // ファイルの先頭でimport
 import Link from "next/link";
-function Bazaar() {
+function exhibition() {
     // SVGがdivで囲まれて埋め込まれる
     // divの大きさを変えることでSVGの大きさも変わる
     return <div dangerouslySetInnerHTML={{ __html: exhibitionIcons["PRパート"] }} />
@@ -43,7 +43,7 @@ export function Exhibition2() {
             <p className={styles.tagiconstamp}>スタンプラリー</p>
             <p className={styles.tagtextstamp}>スタンプラリーを行っている団体です。</p>
             <p className={styles.tagiconevents}>イベント</p>
-            <p className={styles.tagtextevents}>イベントを行っている団体です。詳しくは<span className={styles.underline} >Evens</span>をチェック！</p>
+            <p className={styles.tagtextevents}>イベントを行っている団体です。詳しくは<link><a className={styles.underline} >Evens</a></link>をチェック！</p>
             <p className={styles.tagiconbazaar}>バザー</p>
             <p className={styles.tagtextbazaar}>バザーでの販売を行っている団体です。</p>
         </div>
