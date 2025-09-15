62f92f486d29a15f9920b589f7368a6d2710a805
Rac <rac75116@gmail.com>
2025-09-16 00:36:30 +0900

    ロゴのSVG追加

--- src/app/theme-logo/page.tsx
+++ src/app/theme-logo/page.tsx
@@ -1,5 +1,3 @@
-import styles from "./page.module.scss";
-
 export const metadata = {
     title: "Theme & Logo | 第61回菁々祭「分秒」 - 東大寺学園文化祭2025",
 };
@@ -7,8 +5,13 @@ export const metadata = {
 export default function Page() {
     return (
         <>
-            {/* ここに書く */}
-            <p className={styles.example}>テーマロゴ</p>
+            <h1 className="mt-[30px] w-full px-[20px] text-[28px] font-bold text-[#0b0e0f] md:text-[36px]">
+                <span className="text-[#de0d22]">T</span>heme & <span className="text-[#de0d22]">L</span>ogo
+            </h1>
+            <p className="mt-[25px] w-full px-[20px] text-[16px] text-[#0b0e0f]">
+                テーマ・ロゴの紹介に加え、関連データのダウンロードができるページです。
+            </p>
+            <h2 className="mt-[20px] w-full px-[20px] text-center text-[24px] font-bold text-[#de0d22]">Logotype</h2>
         </>
     );
 }
