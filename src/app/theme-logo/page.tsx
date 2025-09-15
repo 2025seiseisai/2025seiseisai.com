007d6c638befaf7caa193c2b920c2b74146ed7b2
Rac <rac75116@gmail.com>
2025-09-16 00:53:37 +0900

    Theme & Logo 作りかけ

--- src/app/theme-logo/page.tsx
+++ src/app/theme-logo/page.tsx
@@ -1,3 +1,13 @@
+//
+import Link from "next/link";
+import LogoMono from "../../../public/theme-logo/Logo-mono.svg";
+import Logo from "../../../public/theme-logo/Logo.svg";
+import LogotypeMono from "../../../public/theme-logo/Logotype-mono.svg";
+import Logotype from "../../../public/theme-logo/Logotype.svg";
+import TypographyMono from "../../../public/theme-logo/Typography-mono.svg";
+import Typography from "../../../public/theme-logo/Typography.svg";
+import DownloadIcon from "./download-icon.svg";
+
 export const metadata = {
     title: "Theme & Logo | 第61回菁々祭「分秒」 - 東大寺学園文化祭2025",
 };
@@ -12,6 +22,17 @@ export default function Page() {
                 テーマ・ロゴの紹介に加え、関連データのダウンロードができるページです。
             </p>
             <h2 className="mt-[20px] w-full px-[20px] text-center text-[24px] font-bold text-[#de0d22]">Logotype</h2>
+            <div className="relative mt-[15px] w-full px-[20px]">
+                <Logotype className="ml-[3%] h-auto w-[70%]" />
+                <Link href="/theme-logo/Logotype.svg" download className="absolute right-[3%] bottom-0">
+                    <DownloadIcon />
+                </Link>
+            </div>
+            <LogotypeMono />
+            <Logo />
+            <LogoMono />
+            <Typography />
+            <TypographyMono />
         </>
     );
 }
