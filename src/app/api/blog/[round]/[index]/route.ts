48142d7537e50a4a58b6b2ab4f5cc59e33974ecf
Rac <rac75116@gmail.com>
2025-09-28 11:17:40 +0900

    修正

--- src/app/api/blog/[round]/[index]/route.ts
+++ src/app/api/blog/[round]/[index]/route.ts
@@ -13,5 +13,14 @@ export async function GET(request: NextRequest, { params }: { params: Promise<{ 
     if (metadata.title === undefined) {
         return NextResponse.json({ error: "Blog post not found" }, { status: 404 });
     }
-    return NextResponse.json(metadata, { status: 200 });
+    return NextResponse.json(
+        {
+            ...metadata,
+            thumbnail: {
+                ...metadata.thumbnail,
+                src: metadata.thumbnail.src.replace("/server/assets/", "/2025/_next/static/media/"),
+            },
+        },
+        { status: 200 },
+    );
 }
