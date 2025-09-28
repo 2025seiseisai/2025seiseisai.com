0f369bd9e64bffd55f34fadf908147e28d82ffc1
Rac <rac75116@gmail.com>
2025-09-28 13:55:02 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
     images: {
         loader: "custom",
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        imageSizes: [10, 16, 32, 48, 64, 96, 128, 256, 384],
     },
     transpilePackages: ["next-image-export-optimizer"],
     env: {
