109273d867b354d0aab06906202ced5ec151b31e
Rac <rac75116@gmail.com>
2025-09-28 13:24:18 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
         nextImageExportOptimizer_exportFolderPath: "out",
         nextImageExportOptimizer_quality: "75",
         nextImageExportOptimizer_storePicturesInWEBP: "true",
-        nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+        nextImageExportOptimizer_exportFolderName: "2025/static-image",
         nextImageExportOptimizer_generateAndUseBlurImages: "false",
         nextImageExportOptimizer_remoteImageCacheTTL: "0",
     },
