9db5ad4d0d4d6282216794776568434bf9e80e47
mofmof2974 <leonmail2974@gmail.com>
2026-03-24 01:36:27 +0900

    変更

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'export',
     basePath: "/2025",
     assetPrefix: "/2025",
     experimental: {
